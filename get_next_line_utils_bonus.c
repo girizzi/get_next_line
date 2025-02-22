f15b6caca7720b3c2f7709bb4c4fd1190e9ff7b0
Giancarlo Rizzi <girizzi@e3r1p7.42roma.it>
2025-02-22 12:13:32 +0100

    fix: header including

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*line_malloc(int fd, char *str)
 {
